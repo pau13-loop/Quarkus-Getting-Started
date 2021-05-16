d21633faacc6f1516538bd4edd3d4e19dd034996
pau <pllinas@cifpfbmoll.eu>
2021-05-16 19:01:23 +0200

    feat: :sparkles: Change config file to return a msg

--- quarkusgettingstarted/src/main/java/org/acme/quickstart/GreetingResource.java
+++ quarkusgettingstarted/src/main/java/org/acme/quickstart/GreetingResource.java
@@ -5,12 +5,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @Path("/hello")
 public class GreetingResource {
+
+    @ConfigProperty(name = "greetings.message")
+    String msg;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return msg;
     }
 }
